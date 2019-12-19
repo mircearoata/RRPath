51eea2bddadd92604c66cc2926c7688859aa7a33
mircearoata <mircearoatapalade@yahoo.ro>
2019-12-19 23:44:18 +0300

    Pause and scrubbing

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -8,6 +8,8 @@ import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.image.Image
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
@@ -26,8 +28,6 @@ class App : Application() {
     lateinit var fieldImage: Image
     lateinit var stage: Stage
 
-
-    var activeTrajectoryIndex = 0
     val trajectoryDurations = trajectories.map { it.duration() }
     val duration = trajectoryDurations.sum()
     val numberOfTrajectories = trajectories.size
@@ -36,6 +36,8 @@ class App : Application() {
         var WIDTH = 0.0
         var HEIGHT = 0.0
     }
+
+    private var scrubbingSpeed = 1.0
 
     override fun start(stage: Stage?) {
         this.stage = stage!!
@@ -59,6 +61,17 @@ class App : Application() {
             )
         )
 
+        stage.scene.onKeyPressed = EventHandler<KeyEvent> {
+            when (it.code) {
+                KeyCode.SPACE -> paused = !paused
+                KeyCode.LEFT -> runtime -= scrubbingSpeed
+                KeyCode.RIGHT -> runtime += scrubbingSpeed
+                KeyCode.ADD -> scrubbingSpeed *= 2
+                KeyCode.SUBTRACT-> scrubbingSpeed /= 2
+                else -> {}
+            }
+        }
+
         root.children.addAll(canvas, startRect, endRect, robotRect)
 
         stage.title = "PathVisualizer"
@@ -70,7 +83,24 @@ class App : Application() {
         t1.play()
     }
 
+    var runtime = 0.0
+    var prevTime = Clock.seconds
+
+    var paused = false
+
+    fun trajectoriesTime(trajectoryIdx: Int): Double {
+        var x = 0.0
+        for (i in 0 until trajectoryIdx)
+            x += trajectoryDurations[i]
+        return x
+    }
+
     fun run(gc: GraphicsContext) {
+        val deltaTime = Clock.seconds - prevTime
+        prevTime = Clock.seconds
+        if (!paused) {
+            runtime += deltaTime
+        }
         if (startTime.isNaN())
             startTime = Clock.seconds
 
@@ -84,40 +114,34 @@ class App : Application() {
         TrajectoryGen.drawOffbounds()
         gc.globalAlpha = 1.0
 
+        while (runtime >= duration)
+            runtime -= duration
+        while (runtime < 0)
+            runtime += duration
+
+        var activeTrajectoryIndex = 0
+        var prevDurations = 0.0
+        while (activeTrajectoryIndex < numberOfTrajectories && prevDurations + trajectoryDurations[activeTrajectoryIndex] < runtime) {
+            prevDurations += trajectoryDurations[activeTrajectoryIndex]
+            activeTrajectoryIndex++
+        }
+
         val trajectory = trajectories[activeTrajectoryIndex]
 
-        val prevDurations: Double = {
-            var x = 0.0
-            for (i in 0 until activeTrajectoryIndex)
-                x += trajectoryDurations[i]
-            x
-        }()
-
-        val time = Clock.seconds
-        val profileTime = time - startTime - prevDurations
-        val duration = trajectoryDurations[activeTrajectoryIndex]
+        val profileTime = runtime - prevDurations
 
         val start = trajectories.first().start()
         val end = trajectories.last().end()
         val current = trajectory[profileTime]
 
-        if (profileTime >= duration) {
-            activeTrajectoryIndex++
-            if(activeTrajectoryIndex >= numberOfTrajectories) {
-                activeTrajectoryIndex = 0
-                startTime = time
-            }
-        }
-
-        trajectories.forEach{GraphicsUtil.drawSampledPath(it.path)}
+        trajectories.forEach { GraphicsUtil.drawSampledPath(it.path) }
 
         GraphicsUtil.updateRobotRect(startRect, start, GraphicsUtil.END_BOX_COLOR, 0.5)
         GraphicsUtil.updateRobotRect(endRect, end, GraphicsUtil.END_BOX_COLOR, 0.5)
 
         GraphicsUtil.updateRobotRect(robotRect, current, GraphicsUtil.ROBOT_COLOR, 0.75)
         GraphicsUtil.drawRobotVector(current)
-
-        stage.title = "Profile duration : ${"%.2f".format(duration)} - time in profile ${"%.2f".format(profileTime)}"
+        stage.title = "Profile duration : ${"%.2f".format(duration)} - time in profile ${"%.2f".format(profileTime)} - scrubbing speed ${"%.4f".format(scrubbingSpeed)}"
     }
 }
 
