83d2fa6f2ea911a4bbdd3c2e8cb2f4c31e874f8d
mircearoata <mircearoatapalade@yahoo.ro>
2019-12-20 11:57:56 +0300

    On screen data about the trajectory

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -7,58 +7,79 @@ import javafx.scene.Group
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
+import javafx.scene.control.Control
+import javafx.scene.control.Label
 import javafx.scene.image.Image
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
-import javafx.scene.layout.StackPane
+import javafx.scene.layout.ColumnConstraints
+import javafx.scene.layout.GridPane
+import javafx.scene.layout.Priority
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
+import javafx.scene.text.Font
 
 import javafx.stage.Stage
 import javafx.util.Duration
 
-class App : Application() {
-    val robotRect = Rectangle(100.0, 100.0, 10.0, 10.0)
-    val startRect = Rectangle(100.0, 100.0, 10.0, 10.0)
-    val endRect = Rectangle(100.0, 100.0, 10.0, 10.0)
+class App(private val TEXT_SIZE: Double = 20.0) : Application() {
+    private val robotRect = Rectangle(0.0, 0.0, 10.0, 10.0)
+    private val startRect = Rectangle(0.0, 0.0, 10.0, 10.0)
+    private val endRect = Rectangle(0.0, 0.0, 10.0, 10.0)
+    private var canvas: Canvas = Canvas(100.0, 100.0)
+    private var durationText = "Total time: %.2f"
+    private val durationLabel = Label(durationText.format(0.0))
+    private var currentTimeText = "Current time: %.2f"
+    private val currentTimeLabel = Label(currentTimeText.format(0.0))
+    private var trajectoryDurationText = "Trajectory time: %.2f"
+    private val trajectoryDurationLabel = Label(trajectoryDurationText.format(0.0))
+    private var currentTrajectoryTimeText = "Current trajectory time: %.2f"
+    private val currentTrajectoryTimeLabel = Label(currentTrajectoryTimeText.format(0.0))
+    private var scrubbingSpeedText = "Scrubbing speed: %.5f"
+    private val scrubbingSpeedLabel = Label(scrubbingSpeedText.format(0.0))
 
-    var startTime = Double.NaN
-    val trajectories = TrajectoryGen.createTrajectory()
+    private var startTime = Double.NaN
+    private val trajectories = TrajectoryGen.createTrajectory()
 
-    lateinit var fieldImage: Image
-    lateinit var stage: Stage
+    private lateinit var fieldImage: Image
+    private lateinit var stage: Stage
 
-    val trajectoryDurations = trajectories.map { it.duration() }
-    val duration = trajectoryDurations.sum()
-    val numberOfTrajectories = trajectories.size
-
-    companion object {
-        var WIDTH = 0.0
-        var HEIGHT = 0.0
-    }
+    private val trajectoryDurations = trajectories.map { it.duration() }
+    private val duration = trajectoryDurations.sum()
+    private val numberOfTrajectories = trajectories.size
 
     private var scrubbingSpeed = 1.0
+
+    private fun setFieldSize(size: Double) {
+        canvas.width = size
+        canvas.height = size
+        GraphicsUtil.pixelsPerInch = size / GraphicsUtil.FIELD_WIDTH
+        GraphicsUtil.halfFieldPixels = size / 2.0
+    }
 
     override fun start(stage: Stage?) {
         this.stage = stage!!
         fieldImage = Image("/field.png")
 
-        val root = Group()
+        val canvasGroup = Group()
 
-        WIDTH = fieldImage.width
-        HEIGHT = fieldImage.height
-        GraphicsUtil.pixelsPerInch = WIDTH / GraphicsUtil.FIELD_WIDTH
-        GraphicsUtil.halfFieldPixels = WIDTH / 2.0
+        setFieldSize(fieldImage.width)
 
-        val canvas = Canvas(WIDTH, HEIGHT)
         val gc = canvas.graphicsContext2D
         val t1 = Timeline(KeyFrame(Duration.millis(10.0), EventHandler<ActionEvent> { run(gc) }))
         t1.cycleCount = Timeline.INDEFINITE
 
+        val grid = GridPane()
+        grid.hgap = 5.0
+        grid.add(canvasGroup, 0, 0, 3, 1)
+        grid.add(durationLabel, 0, 1)
+        grid.add(currentTimeLabel, 0, 2)
+        grid.add(trajectoryDurationLabel, 2, 1)
+        grid.add(currentTrajectoryTimeLabel, 2, 2)
+        grid.add(scrubbingSpeedLabel, 1, 1)
+
         stage.scene = Scene(
-            StackPane(
-                root
-            )
+            grid
         )
 
         stage.scene.onKeyPressed = EventHandler<KeyEvent> {
@@ -66,36 +87,51 @@ class App : Application() {
                 KeyCode.SPACE -> paused = !paused
                 KeyCode.LEFT -> runtime -= scrubbingSpeed
                 KeyCode.RIGHT -> runtime += scrubbingSpeed
-                KeyCode.ADD -> scrubbingSpeed *= 2
-                KeyCode.SUBTRACT-> scrubbingSpeed /= 2
+                KeyCode.ADD -> {
+                    if (scrubbingSpeed < duration)
+                        scrubbingSpeed *= 2
+                }
+                KeyCode.SUBTRACT-> {
+                    if (scrubbingSpeed > 1e-5)
+                        scrubbingSpeed /= 2
+                }
                 else -> {}
             }
         }
 
-        root.children.addAll(canvas, startRect, endRect, robotRect)
+        canvasGroup.children.addAll(canvas, startRect, endRect, robotRect)
 
         stage.title = "PathVisualizer"
-        stage.isResizable = false
+        stage.isResizable = true
+        stage.widthProperty().addListener { _, _, newW -> setFieldSize(Math.min(newW.toDouble(), stage.height - 100)) }
+        stage.heightProperty().addListener { _, _, newH -> setFieldSize(Math.min(newH.toDouble() - 100, stage.width)) }
+        stage.width = 700.0
+        stage.height = 800.0
 
-        println("duration $duration")
+        durationLabel.font = Font("Arial", TEXT_SIZE)
+        currentTimeLabel.font = Font("Arial", TEXT_SIZE)
+        trajectoryDurationLabel.font = Font("Arial", TEXT_SIZE)
+        currentTrajectoryTimeLabel.font = Font("Arial", TEXT_SIZE)
+        scrubbingSpeedLabel.font = Font("Arial", TEXT_SIZE)
+
+        grid.prefWidthProperty().bind(stage.widthProperty())
+        val columnConstraints = ColumnConstraints(100.0, Control.USE_COMPUTED_SIZE, Double.MAX_VALUE)
+        columnConstraints.hgrow = Priority.ALWAYS
+        grid.columnConstraints.addAll(columnConstraints, columnConstraints, columnConstraints)
+
+        durationLabel.text = durationText.format(duration)
+        currentTimeLabel.text = currentTimeText.format(0.0)
 
         stage.show()
         t1.play()
     }
 
-    var runtime = 0.0
-    var prevTime = Clock.seconds
+    private var runtime = 0.0
+    private var prevTime = Clock.seconds
 
-    var paused = false
+    private var paused = false
 
-    fun trajectoriesTime(trajectoryIdx: Int): Double {
-        var x = 0.0
-        for (i in 0 until trajectoryIdx)
-            x += trajectoryDurations[i]
-        return x
-    }
-
-    fun run(gc: GraphicsContext) {
+    private fun run(gc: GraphicsContext) {
         val deltaTime = Clock.seconds - prevTime
         prevTime = Clock.seconds
         if (!paused) {
@@ -105,7 +141,7 @@ class App : Application() {
             startTime = Clock.seconds
 
         GraphicsUtil.gc = gc
-        gc.drawImage(fieldImage, 0.0, 0.0)
+        gc.drawImage(fieldImage, 0.0, 0.0, canvas.width, canvas.height)
 
         gc.lineWidth = GraphicsUtil.LINE_THICKNESS
 
@@ -141,7 +177,11 @@ class App : Application() {
 
         GraphicsUtil.updateRobotRect(robotRect, current, GraphicsUtil.ROBOT_COLOR, 0.75)
         GraphicsUtil.drawRobotVector(current)
-        stage.title = "Profile duration : ${"%.2f".format(duration)} - time in profile ${"%.2f".format(profileTime)} - scrubbing speed ${"%.4f".format(scrubbingSpeed)}"
+
+        currentTimeLabel.text = currentTimeText.format(runtime)
+        trajectoryDurationLabel.text = trajectoryDurationText.format(trajectoryDurations[activeTrajectoryIndex])
+        currentTrajectoryTimeLabel.text = trajectoryDurationText.format(profileTime)
+        scrubbingSpeedLabel.text = scrubbingSpeedText.format(scrubbingSpeed)
     }
 }
 
