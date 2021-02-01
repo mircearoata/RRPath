f19eba6fb6e1ae69cefc7a8baad05504b2371d42
Mircea Roata <mircearoatapalade@yahoo.ro>
2021-02-01 14:01:04 +0200

    Update to RR 0.5.3

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -2,39 +2,29 @@ import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
-import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
-import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints
-import com.visualizer.AutoTrajectoryGenerator
-import com.visualizer.TrajectoryUtils.*
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint
 
 
 object TrajectoryGen {
-    val ALLIANCE = Alliance.RED
-
-    private val BASE_CONSTRAINTS = DriveConstraints(
-        35.0, 40.0, 0.0,
-        Math.toRadians(180.0), Math.toRadians(180.0), 0.0
-    )
+    private val VELOCITY_CONSTRAINT = MinVelocityConstraint(listOf(AngularVelocityConstraint(30.0), MecanumVelocityConstraint(30.0, 16.0)))
+    private val ACCELERATION_CONSTRAINT = ProfileAccelerationConstraint(30.0)
 
     fun createTrajectory(): ArrayList<Trajectory> {
         val list = ArrayList<Trajectory>()
-        val trajectories = AutoTrajectoryGenerator(ALLIANCE, START_POSE_SIDE)
 
-        for (traj in trajectories.getTrajectoriesSideArm2Stones(AutoTrajectoryGenerator.SkystonePattern.LEFT)) {
-            list.add(traj)
-        }
-
-//        list.add(TrajectoryBuilder(Pose2d(0.0,0.0,0.0), 0.0, BASE_CONSTRAINTS)
-//            .splineToConstantHeading(Pose2d(0.0, 10.0, 0.0))
-//            .splineTo(Pose2d(10.0, 10.0, 0.0))
-//            .build())
+        list.add(TrajectoryBuilder(Pose2d(0.0,0.0,0.0), 0.0, VELOCITY_CONSTRAINT, ACCELERATION_CONSTRAINT)
+            .splineToConstantHeading(Vector2d(0.0, 10.0), 0.0)
+            .splineTo(Vector2d(10.0, 10.0), 0.0)
+            .build())
 
         return list
     }
 
     fun drawOffbounds() {
-        GraphicsUtil.fillRect(flipIfBlue(ALLIANCE, Vector2d(0.0, BRIDGE_OUTER_Y)), 18.0, 18.0) // robot against the wall
-        // GraphicsUtil.fillRect(flipIfBlue(ALLIANCE, Vector2d(0.0,  BRIDGE_INNER_Y)), 18.0, 18.0) // robot inner
+
     }
 }
 
